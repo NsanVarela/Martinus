faba466673ea1b55f50561dd9d8a0cbf4e83dfab
NsanV <nsanv.dev@gmail.com>
2020-08-08 02:53:55 +0200

    carousel works

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import imgMain1 from '../assets/images/imgMain1.jpg';
+import imgMain2 from '../assets/images/imgMain2.jpg';
+import imgMain3 from '../assets/images/imgMain3.jpg';
 
 const useStyles = makeStyles({
     main: {
@@ -8,12 +14,25 @@ const useStyles = makeStyles({
     },
 });
 
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+};
+
 export default function Main() {
     const classes = useStyles();
     return (
-        <div className={classes.main}>
-            <h1>Hi there.</h1>
-            <p>I'm a web developer blah blah blah...</p>
-        </div>
+        <>
+            <div className={classes.main}>
+                <Slider {...settings}>
+                    <img src={imgMain1} width="100%" alt="img 1" />
+                    <img src={imgMain2} width="100%" alt="img 2" />
+                    <img src={imgMain3} width="100%" alt="img 3" />
+                </Slider>
+            </div>
+        </>
     )
 }
