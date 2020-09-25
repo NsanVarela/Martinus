f9d73ec5f4f86e5fbe2864fb32c1e624fd8371d9
NsanV <nsanv.dev@gmail.com>
2020-09-25 17:34:57 +0200

    data events

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -4,9 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Button from '@material-ui/core/Button';
-import imgEvent1 from '../assets/images/events/imgEvent1.jpg';
-import imgEvent2 from '../assets/images/events/imgEvent2.jpg';
-import imgEvent3 from '../assets/images/events/imgEvent3.png';
+import {events} from '../data/Events.js';
 
 const useStyles = makeStyles({
     event: {
@@ -51,24 +49,24 @@ const settings = {
 
 export default function Event() {
     const classes = useStyles();
+
     return (
         <div className={classes.event} id="event">
             <div className={classes.imgContainer}>
                 <Slider {...settings}>
-                    <img className={classes.images} src={imgEvent1} alt="img 1" />
-                    <img className={classes.images} src={imgEvent2} alt="img 2" />
-                    <img className={classes.images} src={imgEvent3} alt="img 3" />
+                    {events.map((item) => {
+                        return (
+                            <>
+                                <img className={classes.images} src={item.imgPath} alt="évènements"/>
+                                <div className={classes.eventDetails}>
+                                    <h3 className={classes.eventTitle}>{item.title}</h3>
+                                    <p className={classes.eventCorps}>{item.description}</p>
+                                    <Button className={classes.registerBtn} variant="contained">je participe à cet évènement</Button>
+                                </div>
+                            </>
+                        );
+                    })}
                 </Slider>
-            </div>
-            <div className={classes.eventDetails}>
-                <h3 className={classes.eventTitle}>JANVIER 2021</h3>
-                <p className={classes.eventCorps}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-                qui officia deserunt mollit anim id est laborum.</p>
-                <Button className={classes.registerBtn} variant="contained">je participe à cet évènement</Button>
             </div>
         </div>
     );
