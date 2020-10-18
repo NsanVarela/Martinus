518919879faf88625596338870cfc8202a9564c6
NsanV <nsanv.dev@gmail.com>
2020-10-18 19:33:54 +0200

    form real time validation

--- src/components/useForm.js
+++ src/components/useForm.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 
-export function useForm(initialFieldsValues) {
+export function useForm(initialFieldsValues, validateOnChange = false, validate) {
 
   const [values, setValues] = useState(initialFieldsValues);
+  const [errors, setErrors] = useState({});
 
   const handleInputChange = e => {
     const { name, value } = e.target
     setValues({
       ...values,
-      [name]:value
+      [name]:value,
     })
-  }
+    if (validateOnChange)
+    validate({ [name]: value })
+  };
+
+  const resetForm = () => {
+    setValues(initialFieldsValues);
+    setErrors({});
+  };
 
   return {
     values,
     setValues,
-    handleInputChange
+    errors,
+    setErrors,
+    handleInputChange,
+    resetForm,
   }
 }
 
@@ -27,14 +38,15 @@ const useStyles = makeStyles(theme => ({
       margin: theme.spacing(1),
     }
   }
-}))
+}));
 
 export function Form(props) {
 
   const classes = useStyles();
+  const { children, ...other } = props;
   return (
-    <form className={classes.root} autoComplete="off">
+    <form className={classes.root} autoComplete="off" {...other}>
       {props.children}
     </form>
   )
-}
+};
--- src/pages/Users/UserForm.jsx
+++ src/pages/Users/UserForm.jsx
@@ -13,48 +13,77 @@ const initialFieldsValues = {
 }
 export default function UserForm() {
 
+  const validate = (fieldValues = values) => {
+    let temp = {...errors}
+    if ('firstName' in fieldValues)
+      temp.firstName = (/^([a-zA-Z\u00C0-\u00FF]+['-]?[a-zA-Z\u00C0-\u00FF]+){1,30}$/).test(fieldValues.firstName) ? `` : `Un prénom facilitera nos échanges`
+    if ('email' in fieldValues)
+      temp.email = (/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/).test(fieldValues.email) ? `` : `Votre email n'est pas valide`
+    setErrors({
+      ...temp
+    })
+
+    if (fieldValues == values)
+      return Object.values(temp).every(x => x == ``)
+  };
+
   const {
     values,
     setValues,
+    errors,
+    setErrors,
     handleInputChange,
-  } = useForm(initialFieldsValues);
+    resetForm,
+  } = useForm(initialFieldsValues, true, validate);
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(validate())
+    window.alert('testing...')
+  };
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Grid container>
         <Grid item xs={3}>
           <Controls.Input
             name="fullName"
             label="Nom"
             value={values.fullName}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+            error={errors.fullName} />
           <Controls.Input
             label="Prénom"
             name="firstName"
             value={values.firstName}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          error={errors.firstName} />
           <Controls.Input
             label="Ville"
             name="city"
             value={values.city}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          error={errors.city} />
           <Controls.Input
             label="Email"
             name="email"
             value={values.email}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          error={errors.email} />
           <Controls.Input 
             label="Téléphone"
             name="mobile"
             value={values.mobile}
-            onChange={handleInputChange} />
+            onChange={handleInputChange}
+          error={errors.mobile} />
           <div>
             <Controls.Button
               type="submit"
               text="Valider" />
             <Controls.Button
-              text="Annuler"
-              color="default" />
+              text="Réinitialiser"
+              color="default"
+              onClick={resetForm} />
           </div>
         </Grid>
         <Grid item xs={6}></Grid>
