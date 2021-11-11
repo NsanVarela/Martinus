36b3a808efe4759b8765891e2d6c429c3c77c4ff
Nicolas SANCHES VARELA <nsanv.dev@gmail.com>
2021-11-11 18:44:58 +0100

    add config webpack for img path

--- webpack.config.js
+++ webpack.config.js
@@ -2,3 +2,24 @@ new HTMLWebpackPlugin({
     template: '/path/to/index.html',
     favicon: '/path/to/favicon.ico',
  })
+
+[{
+    entry: path.resolve(projectRoot, 'src', 'app.ts'),
+    mode: 'production',
+    output: {
+      path: path.resolve(projectRoot, 'dist'),
+      filename: 'app.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(eot|ttf|woff|woff2|svg|png)$/i,
+          use: 'file-loader',
+          options: {
+            esModule: false,
+            },
+        }
+      ]
+    }
+  }
+]
